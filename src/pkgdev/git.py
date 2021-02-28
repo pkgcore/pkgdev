2d412b584a86b8c183e6eafc4fab6c3f6438c9bb
Tim Harder <radhermit@gmail.com>
2021-02-28 10:46:42 -0700

    git: don't capture stderr output for failed git commands

--- src/pkgdev/git.py
+++ src/pkgdev/git.py
@@ -6,12 +6,10 @@ from snakeoil.cli.exceptions import UserException
 def run(*args, **kwargs):
     """Wrapper for running git via subprocess.run()."""
     kwargs.setdefault('check', True)
-    kwargs.setdefault('stderr', subprocess.PIPE)
     kwargs.setdefault('text', True)
     try:
         return subprocess.run(['git'] + list(*args), **kwargs)
     except FileNotFoundError as e:
         raise UserException(str(e))
     except subprocess.CalledProcessError as e:
-        error = e.stderr.splitlines()[0]
-        raise UserException(error)
+        raise SystemExit(e.returncode)
