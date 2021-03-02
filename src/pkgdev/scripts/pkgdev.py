6618ca96dcf28b674a4777e13a5a6f244ec99933
Tim Harder <radhermit@gmail.com>
2021-03-02 13:34:47 -0700

    pkgdev: make snakeoil register known subcommands internally

--- src/pkgdev/scripts/pkgdev.py
+++ src/pkgdev/scripts/pkgdev.py
@@ -6,9 +6,4 @@ pkgdev provides a collection of tools for Gentoo development.
 from pkgcore.util import commandline
 
 argparser = commandline.ArgumentParser(
-    description=__doc__, help=False, script=(__file__, __name__))
-
-subparsers = argparser.add_subparsers()
-subparsers.add_command('commit')
-subparsers.add_command('manifest')
-subparsers.add_command('push')
+    description=__doc__, help=False, subcmds=True, script=(__file__, __name__))
