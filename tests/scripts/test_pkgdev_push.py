41a6301e157d47c84ed564077f934308544cc92b
Tim Harder <radhermit@gmail.com>
2021-03-05 00:06:36 -0700

    tests: add initial `pkgdev push` tests running on git repos

--- tests/scripts/test_pkgdev_push.py
+++ tests/scripts/test_pkgdev_push.py
@@ -1,6 +1,10 @@
 import os
+import textwrap
+from functools import partial
+from unittest.mock import patch
 
 import pytest
+from pkgdev.scripts import run
 from snakeoil.contexts import chdir
 from snakeoil.osutils import pjoin
 
@@ -9,7 +13,7 @@ class TestPkgdevPushParseArgs:
 
     def test_non_repo_cwd(self, capsys, tool):
         with pytest.raises(SystemExit):
-            tool.parse_args(['commit'])
+            tool.parse_args(['push'])
         out, err = capsys.readouterr()
         err = err.strip().split('\n')[-1]
         assert err.endswith('error: not in ebuild repo')
@@ -17,7 +21,7 @@ class TestPkgdevPushParseArgs:
     def test_non_git_repo_cwd(self, repo, capsys, tool):
         with pytest.raises(SystemExit), \
                 chdir(repo.location):
-            tool.parse_args(['commit'])
+            tool.parse_args(['push'])
         out, err = capsys.readouterr()
         err = err.strip().split('\n')[-1]
         assert err.endswith('error: not in git repo')
@@ -27,7 +31,7 @@ class TestPkgdevPushParseArgs:
         repo = make_repo(pjoin(git_repo.path, 'repo'))
         with pytest.raises(SystemExit), \
                 chdir(repo.location):
-            tool.parse_args(['commit'])
+            tool.parse_args(['push'])
         out, err = capsys.readouterr()
         err = err.strip().split('\n')[-1]
         assert err.endswith('error: not in ebuild git repo')
@@ -38,3 +42,68 @@ class TestPkgdevPushParseArgs:
         with chdir(git_repo.path):
             options, _ = tool.parse_args(['push', 'origin', 'master'])
         assert options.push_args == ['origin', 'master']
+
+
+class TestPkgdevPush:
+
+    script = partial(run, 'pkgdev')
+
+    @pytest.fixture(autouse=True)
+    def _setup(self, tmp_path, make_repo, make_git_repo):
+        self.cache_dir = str(tmp_path / 'cache')
+        self.scan_args = ['--scan-args', f'--config no --cache-dir {self.cache_dir}']
+        # args for running pkgdev like a script
+        self.args = ['pkgdev', 'push'] + self.scan_args
+
+        # initialize parent repo
+        self.parent_git_repo = make_git_repo(bare=True)
+        # initialize child repo
+        child_repo_path = tmp_path / 'child-repo'
+        child_repo_path.mkdir()
+        self.child_git_repo = make_git_repo(str(child_repo_path))
+        self.child_repo = make_repo(self.child_git_repo.path)
+        self.child_git_repo.add_all('initial commit')
+        # create a stub pkg and commit it
+        self.child_repo.create_ebuild('cat/pkg-0')
+        self.child_git_repo.add_all('cat/pkg-0')
+        # set up parent repo as origin and push to it
+        self.child_git_repo.run(['git', 'remote', 'add', 'origin', self.parent_git_repo.path])
+        self.child_git_repo.run(['git', 'push', '-u', 'origin', 'master'])
+        self.child_git_repo.run(['git', 'remote', 'set-head', 'origin', 'master'])
+
+    def test_push(self, capsys, repo, make_git_repo):
+        self.child_repo.create_ebuild('cat/pkg-1')
+        self.child_git_repo.add_all('cat/pkg-1')
+
+        with patch('sys.argv', self.args), \
+                pytest.raises(SystemExit) as excinfo, \
+                chdir(self.child_git_repo.path):
+            self.script()
+        assert excinfo.value.code == 0
+
+    def test_failed_push(self, capsys, repo, make_git_repo):
+        self.child_repo.create_ebuild('cat/pkg-1', eapi='-1')
+        self.child_git_repo.add_all('cat/pkg-1')
+
+        # failed scans don't push commits
+        with patch('sys.argv', self.args), \
+                pytest.raises(SystemExit) as excinfo, \
+                chdir(self.child_git_repo.path):
+            self.script()
+        assert excinfo.value.code == 1
+        out, err = capsys.readouterr()
+        assert out == textwrap.dedent("""\
+            cat/pkg
+              InvalidEapi: version 1: invalid EAPI '-1'
+
+            FAILURES
+            cat/pkg
+              InvalidEapi: version 1: invalid EAPI '-1'
+        """)
+
+        # but failures can be ignored to push anyway
+        with patch('sys.argv', self.args + ['--ignore-failures']), \
+                pytest.raises(SystemExit) as excinfo, \
+                chdir(self.child_git_repo.path):
+            self.script()
+        assert excinfo.value.code == 0
