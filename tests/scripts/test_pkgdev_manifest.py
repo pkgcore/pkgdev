ab95e006786381012680943a25a8438558038f76
Tim Harder <radhermit@gmail.com>
2021-03-05 01:31:41 -0700

    tests: add more `pkgdev manifest` restriction parsing tests

--- tests/scripts/test_pkgdev_manifest.py
+++ tests/scripts/test_pkgdev_manifest.py
@@ -2,6 +2,7 @@ from functools import partial
 from unittest.mock import patch
 
 import pytest
+from pkgcore.ebuild.atom import atom as atom_cls
 from pkgdev.scripts import run
 from snakeoil.contexts import chdir
 
@@ -9,11 +10,30 @@ from snakeoil.contexts import chdir
 class TestPkgdevManifestParseArgs:
 
     def test_non_repo_cwd(self, capsys, tool):
-        with pytest.raises(SystemExit):
+        with pytest.raises(SystemExit) as excinfo:
             tool.parse_args(['manifest'])
+        assert excinfo.value.code == 2
         out, err = capsys.readouterr()
         err = err.strip().split('\n')[-1]
         assert err.endswith('error: not in ebuild repo')
+
+    def test_repo_cwd(self, repo, capsys, tool):
+        with chdir(repo.location):
+            options, _ = tool.parse_args(['manifest'])
+        assert options.restrictions
+
+    def test_atom_target(self, repo, capsys, tool):
+        with chdir(repo.location):
+            options, _ = tool.parse_args(['manifest', 'cat/pkg'])
+        assert options.restrictions == [atom_cls('cat/pkg')]
+
+    def test_invalid_atom_target(self, repo, capsys, tool):
+        with pytest.raises(SystemExit) as excinfo, \
+                chdir(repo.location):
+            tool.parse_args(['manifest', '=cat/pkg'])
+        assert excinfo.value.code == 2
+        out, err = capsys.readouterr()
+        err == "pkgdev manifest: error: invalid atom: '=cat/pkg'"
 
 
 class TestPkgdevManifest:
