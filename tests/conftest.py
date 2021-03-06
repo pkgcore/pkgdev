abf6c14bfa6f3141c5b9284a45217d90fc74f24c
Tim Harder <radhermit@gmail.com>
2021-03-06 12:44:56 -0700

    tests: add argparse namespace fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from pkgdev.cli import Tool
 from pkgdev.scripts import pkgdev
+from snakeoil.cli import arghparse
 
 pytest_plugins = ['pkgcore']
 
@@ -16,3 +17,9 @@ def tool():
 def parser():
     """Return a shallow copy of the main pkgdev argparser."""
     return pkgdev.argparser.copy()
+
+
+@pytest.fixture
+def namespace():
+    """Return an arghparse Namespace object."""
+    return arghparse.Namespace()
