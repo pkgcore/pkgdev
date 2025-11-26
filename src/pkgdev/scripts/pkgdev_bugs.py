4a20a162554239137ca6b227407b38ecbf8cd08b
Sam James <sam@gentoo.org>
2025-11-26 18:58:18 +0000

    Revert "bugs: --api-key is now required"
    This reverts commit bb724ed0d883b0dc7522fd263d1447d8aee348a7.
    
    Per request from Arthur on IRC. parona reported it isn't working correctly
    and doesn't handle the arg.
    
    Bug: https://github.com/pkgcore/pkgdev/issues/217
    Signed-off-by: Sam James <sam@gentoo.org>

--- src/pkgdev/scripts/pkgdev_bugs.py
+++ src/pkgdev/scripts/pkgdev_bugs.py
@@ -46,7 +46,7 @@ bugs = ArgumentParser(
     quiet=False,
     parents=(cwd_repo_argparser,),
 )
-BugzillaApiKey.mangle_argparser(bugs, required=True)
+BugzillaApiKey.mangle_argparser(bugs)
 bugs.add_argument(
     "targets",
     metavar="target",
